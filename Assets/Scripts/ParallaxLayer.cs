8dd99e799bf08c87215331f95da26a6686c3413c
Alexander Su <sualexander2013@gmail.com>
2026-04-21 12:01:40 -0700

    parallax
    Former-commit-id: 5a3ce6af286a480bded43e1e6702a7f7b2a113a2

--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -3,43 +3,51 @@ using UnityEngine;
 [DefaultExecutionOrder(100)]
 public class ParallaxLayer : MonoBehaviour
 {
-	[Range(0f, 1f)] public float factor = 0.5f;
+	[Range(0f, 2f)] public float factorMultiplier = 1f;
+	public Transform syncPoint;
 
 	Camera cam;
-	Vector3 anchor;
+	Vector3 restPosition;
+	Vector3 restScale;
 	float initialOrthoSize;
-	bool initialized;
+	bool bIsInitialized;
 
 	void LateUpdate() {
-		if (!initialized && !TryInit()) return;
+		if (!bIsInitialized && !TryInit()) return;
 
+		float globalFactor = Globals.Instance.GetParallaxFactorForZ(restPosition.z);
+		float factor = Mathf.Clamp01(globalFactor * factorMultiplier);
+
+		Vector3 anchorPos = syncPoint != null ? syncPoint.position : restPosition;
+		Vector2 anchorXY = new Vector2(anchorPos.x, anchorPos.y);
 		Vector2 camXY = new Vector2(cam.transform.position.x, cam.transform.position.y);
-		Vector2 anchorXY = new Vector2(anchor.x, anchor.y);
-		Vector2 layerXY = anchorXY + (camXY - anchorXY) * (1f - factor);
+		Vector2 restXY = new Vector2(restPosition.x, restPosition.y);
+		Vector2 layerXY = restXY + (camXY - anchorXY) * (1f - factor);
 
-		transform.position = new Vector3(layerXY.x, layerXY.y, anchor.z);
+		transform.position = new Vector3(layerXY.x, layerXY.y, restPosition.z);
 
 		float zoomRatio = cam.orthographicSize / initialOrthoSize;
 		float counterScale = Mathf.Pow(zoomRatio, 1f - factor);
-		transform.localScale = new Vector3(counterScale, counterScale, 1f);
+		transform.localScale = new Vector3(restScale.x * counterScale, restScale.y * counterScale, restScale.z);
 	}
 
 	bool TryInit() {
-		var follow = FindAnyObjectByType<CameraFollow2D>();
+		CameraFollow2D follow = FindAnyObjectByType<CameraFollow2D>();
 		if (follow == null) return false;
 		cam = follow.GetComponent<Camera>();
 		if (cam == null) return false;
 
-		anchor = new Vector3(cam.transform.position.x, cam.transform.position.y, transform.position.z);
+		restPosition = transform.position;
+		restScale = transform.localScale;
 		initialOrthoSize = Globals.Instance.cameraOrthoSize;
-
-		int count = transform.childCount;
-		Vector3[] worlds = new Vector3[count];
-		for (int i = 0; i < count; i++) worlds[i] = transform.GetChild(i).position;
-		transform.position = anchor;
-		for (int i = 0; i < count; i++) transform.GetChild(i).position = worlds[i];
-
-		initialized = true;
+		bIsInitialized = true;
 		return true;
+	}
+
+	void OnDrawGizmosSelected() {
+		Gizmos.color = new Color(1f, 0.6f, 0.2f, 1f);
+		Gizmos.DrawWireSphere(syncPoint.position, 0.25f);
+		Gizmos.DrawLine(syncPoint.position + Vector3.left * 0.5f, syncPoint.position + Vector3.right * 0.5f);
+		Gizmos.DrawLine(syncPoint.position + Vector3.down * 0.5f, syncPoint.position + Vector3.up * 0.5f);
 	}
 }
